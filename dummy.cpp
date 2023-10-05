465ef576b9e9ba9e8a8503ffd6298f6658733908
ujjwalexcodie <kumarujjwalagrawal4@gmail.com>
2023-10-06 03:32:09 +0530

    Contains files of various Competative platforms

---
+++ dummy.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//Speed
+#define bullet() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+//Macros
+#define loop(i,j) for(int i=0;i<j;i++)
+#define rloop(i,j) for(int i=j-1;i>=0;i--)
+#define st(x) x.begin(), x.end()
+#define py cout<<"YES"<<endl;
+#define pn cout<<"NO"<<endl;
+
+//Typedef
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vec;
+typedef map<int,int> mp;
+typedef unordered_map<int,int> ump;
+typedef vector<vector<int>> dvec;
+typedef vector<pair<int,int>> vec_pair;
+typedef vector<pair<int,pair<int,bool>>> vec_doublepair;
+typedef map<int,int> mp;
+typedef queue<int> q;
+typedef queue<pair<int,int>> doq;
+typedef vector<char> vch;
+
+
+void solve(){
+    cout<<"type"<<endl;
+}
+
+int main(){
+    bullet();
+    int qp;cin>>qp;
+    while(qp--){
+        solve();
+    }
+        return 0;
+}
