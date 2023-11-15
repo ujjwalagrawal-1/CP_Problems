6dd6afb8568369a16171a6539cf26f0e913f2c4b
ujjwalexcodie <kumarujjwalagrawal4@gmail.com>
2023-11-15 20:28:23 +0530

    some more probems

--- A_Bit.cpp
+++ A_Bit.cpp
@@ -13,7 +13,7 @@ using namespace std;
 //Macros
 #define IOtext freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
 #define Pai (3.141592653589)
-#define M 1000000007
+#define M 1000000007  
 #define in(x) insert(x);
 #define pb push_back
 #define pp pop_back
@@ -43,11 +43,12 @@ using namespace std;
 #define cn cout<<"NO"<<endl
 #define cm cout<<"-1"<<endl
 #define ps(x,y) fixed<<setprecision(y)<<x
+// #define pb(x) x.push_back()
 
 //Typedef
 typedef long long ll;
 typedef pair<int, int> pi;
-typedef vector<int> vec;
+typedef vector<string> vec;
 typedef vector<char> vec_ch;
 typedef map<int,int> mp;
 typedef unordered_map<int,int> ump;
@@ -114,7 +115,8 @@ ll modinv(ll p,ll q){ll ex;ex=M-2;while (ex) {if (ex & 1) {p = (p * q) % M;}q = 
 void solve()
 {
     inint(x);
-    vector<string> veci(x);
+    vec veci(x);
+    // vec veci(x);
     int u = 0;
     rep(i,x){
         cin>>veci[i];
@@ -133,16 +135,19 @@ void solve()
 int32_t main()
 {
     bullet()
-    #ifndef ONLINE_JUDGE
-        freopen("Error.txt","w",stderr);
-    #endif
-    //God knows when to help you So Keep Giving up your effort bcoz 
-    //when effort and help combine then such erra will come in Which you can't Imagine
-    //              ☆*: .｡. o(≧▽≦)o .｡.:*☆
-    int t = 1;
-    while(t--)
-    {
-    solve();
-    }
-    return 0;
+    // #ifndef ONLINE_JUDGE
+    //     freopen("Error.txt","w",stderr);
+    // #endif
+    // //God knows when to help you So Keep Giving up your effort bcoz 
+    // //when effort and help combine then such erra will come in Which you can't Imagine
+    // //              ☆*: .｡. o(≧▽≦)o .｡.:*☆
+    // int t = 1;
+    // while(t--)
+    // {
+    // solve();
+    // }
+    // return 0;
+    cy;
+    cn;
+
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+bullet()
+    // #ifndef ONLINE_JUDGE
+    //     freopen("Error.txt","w",stderr);
+    // #endif
+    // //God knows when to help you So Keep Giving up your effort bcoz 
+    // //when effort and help combine then such erra will come in Which you can't Imagine
+    // //              ☆*: .｡. o(≧▽≦)o .｡.:*☆
+    // int t = 1;
+    // while(t--)
+    // {
+    // solve();
+    // }
+    // return 0;
